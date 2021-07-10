d4658a78f5171cf7140ef66133930f5c6b982393
M Azhar Alauddin <muhammadazharalauddin@gmail.com>
2021-07-10 22:42:25 +0700

    Week13

---
+++ Week13_TekproPrak/ATMMain.java
@@ -0,0 +1,9 @@
+package iniPackage;
+
+public class ATMMain {
+   // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+   }
+} 
